e42983a8271a12246eff5453b49bf77ace817db6
nminhkhoi0818 <nminhkhoi0818@gmail.com>
2026-01-19 13:54:24 +0700

    feat: add API get onboarding questions and set up docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { RecommendationsModule } from './recommendations/recommendations.module';
 import { PlacesModule } from './places/places.module';
+import { QuestionsModule } from './questions/questions.module';
+import { RecommendationsModule } from './recommendations/recommendations.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, RecommendationsModule, PlacesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST ?? 'localhost',
+        port: Number(process.env.POSTGRES_PORT ?? 5432),
+        username: process.env.POSTGRES_USER ?? 'lotara',
+        password: process.env.POSTGRES_PASSWORD ?? 'lotara',
+        database: process.env.POSTGRES_DB ?? 'lotara',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    UsersModule,
+    RecommendationsModule,
+    PlacesModule,
+    QuestionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
